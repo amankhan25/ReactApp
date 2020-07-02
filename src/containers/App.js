6a6d5451b37b455cb76cded059819827e39a7634
Aman <aman.kroolgrabstarz@gmail.com>
2020-07-02 19:35:46 +0530

    qq7 comp

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,10 @@ import Cockpit from '../components/Cockpit/Cockpit'
 //import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -14,6 +18,21 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount(){
+    console.log('[App.js] componentWillMount');
+    
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componnentDidMount');
+    
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -47,6 +66,8 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
+    
     let persons = null;
 
     if (this.state.showPersons) {
